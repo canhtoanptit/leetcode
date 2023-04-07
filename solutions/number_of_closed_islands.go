40fa877c2a433e7d121a5264d47ecc6ac356d017
toannguyen <canhtoanptit@gmail.com>
2023-04-07 12:39:47 +0700

    number of closed insland

---
+++ solutions/number_of_closed_islands.go
@@ -0,0 +1,35 @@
+package leetcode
+
+func closedIsland(grid [][]int) int {
+	// find number of island
+	// loop every island is close island
+	var result int
+	for i := 1; i < len(grid)-1; i++ {
+		for j := 1; j < len(grid)-1; j++ {
+			if grid[i][j] == 0 && flood(grid, i, j) {
+				result++
+			}
+		}
+	}
+
+	return result
+}
+
+func flood(grid [][]int, i, j int) bool {
+	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[i]) {
+		return false
+	}
+
+	if grid[i][j] == 1 {
+		return true
+	}
+
+	grid[i][j] = 1
+
+	up := flood(grid, i-1, j)
+	down := flood(grid, i+1, j)
+	left := flood(grid, i, j-1)
+	right := flood(grid, i, j+1)
+
+	return up && down && left && right
+}
